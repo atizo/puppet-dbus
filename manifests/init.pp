6724ebfb2f7ffc1c1212fe1a4afe3bd3941aa16c
Marcel Haerry <haerry@puzzle.ch>
2008-04-30 10:57:59 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# dbus module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "dbus": }
+class dbus {}
